990d0055dd3172d11479acb26b735c8cc0a1b880
ellingjp <ellingjp@uw.edu>
2014-01-14 17:45:09 -0800

    Code cleanup

--- lab1/part3-app2.c
+++ lab1/part3-app2.c
@@ -22,7 +22,7 @@
 //
 //*****************************************************************************
 
-
+#include "inc/hw_types.h"
 #include "driverlib/debug.h"
 #include "driverlib/sysctl.h"
 #include "drivers/rit128x96x4.h"
@@ -43,96 +43,52 @@ __error__(char *pcFilename, unsigned long ulLine)
 
 #define TRUE 1
 #define FALSE 0
+#define OLEDCLK 1000000
+#define CHARWIDTH 6
 
-
-void delay(int aValue);
+void delay(unsigned long aValue);
 
 //*****************************************************************************
 //
-// Print A then erase then B then erase then C then erase then D then erase
-// to the OLED on the Stellaris evaluation board.
+// Display "ABCD", then remove each letter sequentially.
 //
 //*****************************************************************************
 int
 main(void)
 {
-//
+    unsigned long flash_delay = 8000;   // Delay between flashes (approx 1 sec)
+    
+    char *characters = "ABCD";          // Characters to flash (if changed,
+                                        // must change len)
+    
+    unsigned int len = 4;               // length of characters (excluding 
+                                        // null term)
+    
     // Set the clocking to run directly from the crystal.
-    //
     SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN |
                    SYSCTL_XTAL_8MHZ);
 
-    //
-    // Initialize the OLED display.
-    //
-    RIT128x96x4Init(1000000);
+    // Initialize display
+    RIT128x96x4Init(OLEDCLK);
     
-    //  define some local variables
-	int delayVal = 1000;
-	char A[1];
-	A[0] = 'A';
-	A[1] = '/0';
-    char B[1];
-	B[0] = 'B';
-	B[1] = '/0';      
-	char C[1];
-	C[0] = 'C';
-	C[1] = '/0';      
-	char D[1];
-	D[0] = 'D';
-	D[1] = '/0';
-	char space[1];
-	space[0] = ' ';
-	space[1] = '/0';
-    
-
-    //  print A then B then C then D
+    // Display ABCD, then remove A then B then C then D
+    unsigned int xVal = 15;
+    unsigned int yVal = 15;
     while(TRUE)
     {
-
-	 
-	  RIT128x96x4StringDraw(A, 10, 24, 15);
-	  delay(delayVal);
-	  RIT128x96x4StringDraw(B, 20, 24, 15);
-	  delay(delayVal);
-	  RIT128x96x4StringDraw(C, 30, 24, 15);
-	  delay(delayVal);
-	  RIT128x96x4StringDraw(D, 40, 24, 15);
-	  delay(delayVal);
-	  RIT128x96x4StringDraw(space, 10, 24, 15);
-	  delay(delayVal);
-	  RIT128x96x4StringDraw(space, 20, 24, 15);
-	  delay(delayVal);
-	  RIT128x96x4StringDraw(space, 30, 24, 15);
-	  delay(delayVal);
-	  RIT128x96x4StringDraw(space, 40, 24, 15);
-	  delay(delayVal);
-	  
-	  
-	  
-	  
-	  
+      RIT128x96x4StringDraw(characters, xVal, yVal, 15);
+      delay(flash_delay);
       
-
-
-
-
+      for (volatile unsigned int i = 0; i < CHARWIDTH*len; i+=CHARWIDTH) {
+        RIT128x96x4StringDraw(" ", xVal+i, yVal, 15);
+        delay(flash_delay);
+      }
+    }
 }
 
-
-
-
-//  software delay
-void delay(int aValue)
+// Software delay
+void delay(unsigned long aValue)
 {
-    volatile int i = 0;
-
-    volatile unsigned int j = 0;
-    
-    for (i = aValue; i > 0; i--)
-    {
-        for (j = 0; j < 100; j++);
-    }
-
-    return;
+    for (volatile unsigned int i = 0; i < aValue; i++)
+      for (volatile unsigned int j = 0; j < 100; j++);
 }
