e76b5a3ffb6d1768e0332b15dc647574a283118a
unknown <ellingjp@EE-345PC04.ee.washington.edu>
2014-02-07 15:36:25 -0800

    Updated to work with new globals

--- lab3/code/measure.c
+++ lab3/code/measure.c
@@ -7,6 +7,7 @@
  */
 
 #include "task.h"
+#include "CircularBuffer.h"
 #include "globals.h"
 #include "timebase.h"
 #include "measure.h"
@@ -25,15 +26,10 @@ void measureRunFunction(void *dataptr);
 
 // Internal data structure
 typedef struct measureData {
-  int *tRawBuf;
-  int *tRawHead;
-
-  int *bpRawBuf;
-  int *bpRawSysHead;
-  int *bpRawDiaHead;
-
-  int *prRawBuf;
-  int *prRawHead;
+  CircularBuffer *temperatureRaw;
+  CircularBuffer *systolicPressRaw;
+  CircularBuffer *diastolicPressRaw;
+  CircularBuffer *pulseRateRaw;
 } MeasureData;
 
 static MeasureData data;  // internal data
@@ -44,39 +40,38 @@ void initializeMeasureTask() {
   RIT128x96x4Init(1000000);
 #endif
   // Load data memory
-  data.tRawBuf = &(global.tRawBuf[0]);
-  data.tRawHead = &(global.tRawHead);
-
-  data.bpRawBuf = &(global.bpRawBuf[0]);
-  data.bpRawSysHead = &(global.bpRawSysHead);
-  data.bpRawDiaHead = &(global.bpRawDiaHead);
-
-  data.prRawBuf = &(global.prRawBuf[0]);
-  data.prRawHead = &(global.prRawHead);
+  data.temperatureRaw = &(global.temperatureRaw);
+  data.systolicPressRaw = &(global.systolicPressRaw);
+  data.diastolicPressRaw = &(global.diastolicPressRaw);
+  data.pulseRateRaw = &(global.pulseRateRaw);
 }
 
-void setTemp(int *temp) {
+void setTemp(CircularBuffer *tbuf) {
     static unsigned int i = 0;
     static tBoolean goingUp = true;
 
-    if (*temp > 50)
+    int temp = *(int *)cbGet(tbuf);
+
+    if (temp > 50)
       goingUp = false;
-    else if (*temp < 15)
+    else if (temp < 15)
       goingUp = true;
 
     if (goingUp) {
-        if (i%2==0) (*temp)+=2;
-        else (*temp)--;
+        if (i%2==0) temp+=2;
+        else temp--;
     }
     else {
-        if (i%2==0) (*temp)-=2;
-        else (*temp)++;
+        if (i%2==0) temp-=2;
+        else temp++;
     }
+
+    cbAdd(tbuf, &temp);
     
     i++;
 }
 
-void setSysPress(int *syspress) {
+void setSysPress(CircularBuffer *spbuf) {
     // This is written to lab spec, with a flag to indicate "complete".
     // Right now, it does nothing, but I imagine it should probably be a global
     // variable to indicate to the compute task that the pressure measurement 
@@ -85,52 +80,63 @@ void setSysPress(int *syspress) {
     static unsigned int i = 0;
 
     tBoolean complete = false;    
+    int syspress = *(int *)cbGet(spbuf);
     
-    if (*syspress > 100) {
+    if (syspress > 100) {
       complete = true;
-      *syspress = SYS_RAW_INIT;
+      syspress = SYS_RAW_INIT;
     }
 
-    if (i%2==0) (*syspress)+=3;
-    else (*syspress)--;
+    if (i%2==0) syspress+=3;
+    else syspress--;
+
+    cbAdd(spbuf, &syspress);
     
     i++;
 }
 
-void setDiaPress(int *diapress) {
+void setDiaPress(CircularBuffer *dpbuf) {
     static unsigned int i = 0;
 
     tBoolean complete = false;
-    if (*diapress < 40) {
+    int diapress = *(int *)cbGet(dpbuf);
+
+    if (diapress < 40) {
       complete = true;
-      *diapress = DIA_RAW_INIT;
+      diapress = DIA_RAW_INIT;
     }
     
-    if (i%2==0) (*diapress)-=2;
-    else (*diapress)++;
+    if (i%2==0) diapress-=2;
+    else diapress++;
+
+    cbAdd(dpbuf, &diapress);
     
     i++;
 }
 
-void setPulse(int *pulse) {
+void setPulse(CircularBuffer *pbuf) {
     static unsigned int i = 0;
 
     static tBoolean goingUp = true;
 
-    if (*pulse < 15)
+    int pulse = *(int *)cbGet(pbuf);
+
+    if (pulse < 15)
       goingUp = true;
-    else if (*pulse > 40)
+    else if (pulse > 40)
       goingUp = false;
 
     if (goingUp) {
-      if (i%2 == 0) (*pulse)--;
-      else (*pulse)+=3;
+      if (i%2 == 0) pulse--;
+      else pulse+=3;
     }
     else {
-      if (i%2 == 0) (*pulse)++;
-      else (*pulse)-=3;
+      if (i%2 == 0) pulse++;
+      else pulse-=3;
     }
     
+    cbAdd(pbuf, &pulse);
+
     i++;
 }
 
@@ -146,10 +152,10 @@ void measureRunFunction(void *dataptr) {
   if (IS_MAJOR_CYCLE) {   // on major cycle
     MeasureData *data = (MeasureData *) dataptr;
 
-    setTemp(data);
-    setSysPress(data);
-    setDiaPress(data);
-    setPulse(data);
+    setTemp(data->temperatureRaw);
+    setSysPress(data->systolicPressRaw);
+    setDiaPress(data->diastolicPressRaw);
+    setPulse(data->pulseRateRaw);
     
 #if DEBUG
     char num[30];
