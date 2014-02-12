78b979bff458f8ccada471da369cdba9e4961c9f
ganparse <jarrett155@hotmail.com>
2014-02-12 01:15:46 -0800

    and more spelling

--- lab3/code/keyPad.h
+++ lab3/code/keyPad.h
@@ -1,17 +1,17 @@
 /*
  * keyPad.h
  * Author(s): Jarrett Gaddy
- * 1/28/2014
+ * 2/10/2014
  *
  * Defines the public interface for the keyPad task
  * 
- * initializeStatusTask() should be called once before performing runkeyPad
+ * initializeKeyPadTask() should be called once before performing runkeyPad
  * functions
  */
 
 #include "task.h"  // for TCBs
 
-/* Initialize StatusData, must be done before running functions */
+/* Initialize KeyPadData, must be done before running functions */
 void initializeKeyPadTask();
 
 /* The keyPad Task */
